59244c146a8f3777fdca548b100bebe65431709d
DaniloCecci <danilocecci@gmail.com>
2024-02-09 16:13:30 -0300

    Aula 02

---
+++ src/http/routes/vote-on-poll.ts
@@ -0,0 +1,65 @@
+import { z } from "zod"
+import { prisma } from "../../lib/prisma"
+import { FastifyInstance } from "fastify"
+import { randomUUID } from "crypto"
+
+export async function voteOnPoll(app: FastifyInstance) {
+    app.post('/polls/:pollId/votes', async (request, reply) => {
+        const voteOnPollBody = z.object({
+            pollOptionId: z.string().uuid()
+        })
+
+        const voteonPollParams = z.object({
+            pollId: z.string().uuid()
+        })
+    
+        const { pollOptionId } = voteOnPollBody.parse(request.body)
+        const { pollId } = voteonPollParams.parse(request.params)
+    
+        let { sessionId } = request.cookies
+
+        if (sessionId) {
+            const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+                where: {
+                    sessionId_pollId: {
+                        sessionId,
+                        pollId
+                    }
+                }
+            })
+
+            if(userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId !== pollOptionId) {
+                await prisma.vote.delete({
+                    where: {
+                        id: userPreviousVoteOnPoll.id
+                    }
+                })
+            } else if (userPreviousVoteOnPoll){
+                return reply.status(400).send({"message":"You already vote on this poll."})
+            }
+        }
+
+
+        if (!sessionId) {
+            sessionId = randomUUID()
+    
+            reply.setCookie('sessionId', sessionId, {
+                path: '/',
+                maxAge: 60 * 60 * 24* 30,
+                signed: true,
+                httpOnly: true
+            })
+        }
+
+        await prisma.vote.create({
+            data: {
+                sessionId,
+                pollId,
+                pollOptionId
+            }
+        })
+
+
+        return reply.status(201).send({sessionId})
+    })
+}
